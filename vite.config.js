7cd17d3716db816dd1bca1879e3934aaadb26e5d
ryk765 <mr95721ryk@gmail.com>
2026-03-13 23:09:44 +0900

    fix github pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    base: '/my-homepage/',
+    build: {
+    outDir: 'docs'
+  }
 });
